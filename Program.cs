51c2c1f39279fac06b164c10078c42deb503e1ab
Lesha07 <simchuklesha@gmail.com>
2024-10-18 21:41:50 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Введите первое число");
+int firstnumber = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+int lastnumber = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите математическое действие(+, -, /, *)");
+char userInput = char.Parse(Console.ReadLine());
+
+if (userInput == '+')
+{ 
+    Console.WriteLine(firstnumber + lastnumber);
+}
+else if (userInput == '-')
+{
+  Console.WriteLine(firstnumber - lastnumber);
+}
+else if (userInput == '*')
+{
+    Console.WriteLine(firstnumber * lastnumber);
+}
+else if (userInput == '/')
+{
+    Console.WriteLine(firstnumber / lastnumber);
+}
+else
+{
+    Console.WriteLine("error input");
+}
